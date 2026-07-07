80b458627b5d5917bc3e5925be813315f4fcef72
Shubham Khandekar <mrethical8@gmail.com>
2026-07-07 20:42:49 +0530

    Create Spring Boot backend (shx-core)

---
+++ backend/shx-core/src/test/java/com/shx/core/ShxCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shx.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShxCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
